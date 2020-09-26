25115714d5eb84ec597d8e81ff9fa36351b18417
Кирилюк <kk87.work@gmail.com>
2020-09-26 16:32:52 +0300

    finish style

--- client/components/main.js
+++ client/components/main.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div className="flex justify-center items-center py-2">
+    <div className="flex flex-col justify-center items-center py-2">
       <input
-        className="appearance-none bg-transparent border-b border-teal-500 w-1/3 text-gray-700 mr-3 py-2 px-2 leading-tight focus:outline-none"
+        className="bg-transparent border-b border-teal-500 w-1/3 text-gray-700 py-2 m-2 leading-tight focus:outline-none"
         type="text"
         placeholder="начните поиск"
       />
       <button
-        className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+        className="bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white px-4 py-1 m-2 rounded"
         type="button"
       >
         Search
--- client/components/text-of-readme.js
+++ client/components/text-of-readme.js
@@ -2,18 +2,11 @@ import React from 'react'
 
 const TextOfReadme = () => {
   return (
-    <div className="flex justify-center items-center py-2">
-      <input
-        className="appearance-none bg-transparent border-b border-teal-500 w-1/3 text-gray-700 mr-3 py-2 px-2 leading-tight focus:outline-none"
-        type="text"
-        placeholder="начните поиск"
-      />
-      <button
-        className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
-        type="button"
-      >
-        TextOfReadme
-      </button>
+    <div role="alert" className="flex flex-col justify-center items-center py-2">
+      <div className="w-1/3 bg-teal-500 text-white font-bold rounded-t px-4 py-2">ReadMe:</div>
+      <div className="w-1/3 border border-t-0 border-teal-500 rounded-b px-4 py-3 text-gray-700">
+        <p>Text of Readme</p>
+      </div>
     </div>
   )
 }
