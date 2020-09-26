c553adea9a8fab24bce37f44dad259307801369c
Кирилюк <kk87.work@gmail.com>
2020-09-26 16:56:39 +0300

    add axios

--- client/components/app.js
+++ client/components/app.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Head from './head'
 import Main from './main'
 import ListOfRepos from './list-of-repos'
 import TextOfReadme from './text-of-readme'
 
 const App = () => {
+  const [userName, setUserName] = useState('')
+
   return (
     <div>
       <Head title="Hello" />
-      <Main />
+      <Main setUserName={setUserName} />
       <ListOfRepos />
       <TextOfReadme />
+      {userName}
     </div>
   )
 }
--- client/components/main.js
+++ client/components/main.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Main = () => {
+const Main = ({ setUserName }) => {
+  const [value, setValue] = useState('')
+
+  const controlInput = (event) => {
+    const newValue = event.target.value
+    setValue(newValue)
+  }
+
+  const getUserName = () => {
+    setUserName(value)
+    setValue('')
+  }
+
   return (
     <div className="flex flex-col justify-center items-center py-2">
       <input
         className="bg-transparent border-b border-teal-500 w-1/3 text-gray-700 py-2 m-2 leading-tight focus:outline-none"
         type="text"
-        placeholder="начните поиск"
+        value={value}
+        onChange={controlInput}
       />
       <button
         className="bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white px-4 py-1 m-2 rounded"
         type="button"
+        onClick={getUserName}
       >
         Search
       </button>
